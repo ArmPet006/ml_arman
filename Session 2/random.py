73d7c76c567b1a9c024492e01c6bab087606fa94
ArmPet006 <arman.petrosyan1.g@tumo.org>
2023-07-03 16:59:35 +0100

    OpenCv

--- Session 2/random.py
+++ Session 2/random.py
@@ -1,3 +1,28 @@
 import numpy as np
-np.random.seed(42)
-np.random.random((1,10))
+
+random_array = np.random.randint(0, 100, size = 10)
+print("The array: ", random_array)
+
+mean = np.mean(random_array)
+print("Mean: ", mean)
+
+max_val = np.max(random_array)
+print("Max: ", max_val)
+
+min_val = np.min(random_array)
+print("Min: ", min_val)
+
+array_sum = np.sum(random_array)
+print("Sum: ", array_sum)
+
+sorted_array = np.sort(random_array)
+print("Sorted array: ", sorted_array)
+
+shuffled_array = np.random.permutation(random_array)
+print("Shuffled array: ", shuffled_array)
+
+squared_array = np.square(random_array)
+print("Squared array: ", squared_array)
+
+sqrt_array = np.sqrt(random_array)
+print("Square root array: ", sqrt_array)
